e037cbe5d22be1a00e8402307cb61e5b78eacdd0
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-08-17 12:29:38 +0500

    inst btstrap

---
+++ chat/src/utils/validate.js
@@ -0,0 +1,11 @@
+import * as yup from 'yup';
+
+export const loginSchema = yup.object().shape({
+  userName: yup.string()
+    .required('Обязательное поле')
+    .min(5, 'Минимум 5 букв')
+    .max(50, 'Максимум 50 букв'),
+  password: yup.string()
+    .min(5, 'Минимум 5 символов')
+    .max(50, 'Максимум 50 символов')
+});
